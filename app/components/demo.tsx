5018ef619d147d79e7528375673721773a064d0a
Sataca <glassg767@gmail.com>
2025-11-22 07:48:06 +0200

    committed

--- app/components/demo.tsx
+++ app/components/demo.tsx
@@ -6,7 +6,7 @@ export default function Demo() {
         <div className={style.demo}>
             <h1 className={style.titulo_colorido}>Construa, vende, compre no melhor lugar com melhores preços do mercado</h1>
 
-            <div className={`${style.caixa} font-Bold`}>
+            <div className={`${style.caixa} font-[Archivo]`}>
                <div className={`${style.card} text-2xl bg-blue-800 text-white`}>Card</div>
                <div className={`${style.card} `}>Center</div>
                <div className={`${style.card} `}>Right</div>
